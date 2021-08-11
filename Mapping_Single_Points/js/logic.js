ace89a1a37ec1d2e775fd7feadd2c8a6ec8ff084
Thirumalairajan <Sri.Thirumalairajan@thomsonreuters.com>
2021-08-10 21:06:27 -0400

    changes to logic.js, adding circle map

--- Mapping_Single_Points/js/logic.js
+++ Mapping_Single_Points/js/logic.js
@@ -2,12 +2,14 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+let map = L.map("mapid").setView([34.0522, -118.2437], 14);
+
+  //  Add a marker to the map for Los Angeles, California.
+  L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+ }).addTo(map);
 
   // We create the tile layer that will be the background of our map.
   let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
